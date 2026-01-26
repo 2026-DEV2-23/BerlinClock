45ce9ccb21790565b08cc2ee88a05990bd3ca798
2026-DEV2-23 <6wn65x6wns@privaterelay.appleid.com>
2026-01-26 16:06:15 +0530

    add test case for even numbers from 0 to 58 using arguments

--- BerlinClockTests/BerlinClockEngineTests.swift
+++ BerlinClockTests/BerlinClockEngineTests.swift
@@ -5,9 +5,8 @@ import Testing
 struct BerlinClockEngineTests {
     let engine = BerlinClockEngine()
     
-    @Test("Seconds lamp is on for even seconds")
-    func secondsLampIsOnForEvenSeconds() {
-        let seconds = 20
+    @Test("Seconds lamp is on for even seconds", arguments: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58])
+    func secondsLampIsOnForEvenSeconds(seconds: Int) {
         let original = engine.isSecondsLampOn(seconds: seconds)
         #expect(original == true, "Failed at seconds \(seconds)")
     }
