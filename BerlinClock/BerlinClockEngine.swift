0fd3fce6876c472af35d1434964bb0185fd69343
2026-DEV2-23 <6wn65x6wns@privaterelay.appleid.com>
2026-01-26 20:32:39 +0530

    refactor code fiveMinutesToLamp func

--- BerlinClock/BerlinClockEngine.swift
+++ BerlinClock/BerlinClockEngine.swift
@@ -27,31 +27,12 @@ class BerlinClockEngine {
         guard (0...59).contains(minutes) else {
             throw TimeValidationError.invalidMinutes(minutes)
         }
-        switch minutes {
-        case 5, 6, 7, 8, 9:
-            return [.yellow, .off, .off, .off, .off, .off, .off, .off, .off, .off, .off]
-        case 10, 11, 12, 13, 14:
-            return [.yellow, .yellow, .off, .off, .off, .off, .off, .off, .off, .off, .off]
-        case 15, 16, 17, 18, 19:
-            return [.yellow, .yellow, .red, .off, .off, .off, .off, .off, .off, .off, .off]
-        case 20, 21, 22, 23, 24:
-            return [.yellow, .yellow, .red, .yellow, .off, .off, .off, .off, .off, .off, .off]
-        case 25, 26, 27, 28, 29:
-            return [.yellow, .yellow, .red, .yellow, .yellow, .off, .off, .off, .off, .off, .off]
-        case 30, 31, 32, 33, 34:
-            return [.yellow, .yellow, .red, .yellow, .yellow, .red, .off, .off, .off, .off, .off]
-        case 35, 36, 37, 38, 39:
-            return [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .off, .off, .off, .off]
-        case 40, 41, 42, 43, 44:
-            return [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow, .off, .off, .off]
-        case 45, 46, 47, 48, 49:
-            return [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow, .red, .off, .off]
-        case 50, 51, 52, 53, 54:
-            return [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .off]
-        case 55, 56, 57, 58, 59:
-            return [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow]
-        default:
-            return [.off, .off, .off, .off, .off, .off, .off, .off, .off, .off, .off]
+        let lamps = minutes / 5
+        return (0..<11).map {
+            if $0 < lamps {
+                return ($0 + 1) % 3 == 0 ? .red : .yellow
+            }
+            return .off
         }
     }
 }
