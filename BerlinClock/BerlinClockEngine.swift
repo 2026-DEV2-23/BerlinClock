afb0bbad93dbedafcb722d65e171d4f685c87832
2026-DEV2-23 <6wn65x6wns@privaterelay.appleid.com>
2026-01-26 16:26:06 +0530

    add test for seconds > 60, implement logic in engine, and update existing tests

--- BerlinClock/BerlinClockEngine.swift
+++ BerlinClock/BerlinClockEngine.swift
@@ -1,8 +1,13 @@
 class BerlinClockEngine {
     
-    func isSecondsLampOn(seconds: UInt) -> Bool {
-        seconds % 2 == 0
+    func isSecondsLampOn(seconds: UInt) -> (isEven: Bool?, error: String?) {
+        if seconds > 59 {
+            let error = "Invalid seconds: \(seconds). Seconds should not be more than 59."
+            return (nil, error)
+        } else {
+            let isEven = (seconds % 2 == 0)
+            return (isEven, nil)
+        }
     }
     
 }
-
--- BerlinClockTests/BerlinClockEngineTests.swift
+++ BerlinClockTests/BerlinClockEngineTests.swift
@@ -7,13 +7,21 @@ struct BerlinClockEngineTests {
     
     @Test("Seconds lamp is on for even seconds", arguments: Array(stride(from: 0, to: 60, by: 2)))
     func secondsLampIsOnForEvenSeconds(seconds: UInt) {
-        let original = engine.isSecondsLampOn(seconds: seconds)
+        let original = engine.isSecondsLampOn(seconds: seconds).isEven
         #expect(original == true, "Failed at seconds \(seconds)")
     }
     
     @Test("Seconds lamp is off for odd seconds", arguments: Array(stride(from: 1, to: 60, by: 2)))
     func secondsLampIsOffForEvenSeconds(seconds: UInt) {
-        let original = engine.isSecondsLampOn(seconds: seconds)
+        let original = engine.isSecondsLampOn(seconds: seconds).isEven
         #expect(original == false, "Failed at seconds \(seconds)")
+    }
+    
+    @Test("Seconds lamp should not be more than 59")
+    func secondsLampShouldNotBeMoreThan59() {
+        let seconds: UInt = 61
+        let (isEven, error) = engine.isSecondsLampOn(seconds: seconds)
+        #expect(isEven == nil)
+        #expect(error  == "Invalid seconds: \(seconds). Seconds should not be more than 59.")
     }
 }
