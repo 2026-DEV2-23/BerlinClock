fb0fd4cc9c431682b6c373467bcce40a368ed275
2026-DEV2-23 <6wn65x6wns@privaterelay.appleid.com>
2026-01-26 23:54:33 +0530

    refactor code: MockBerlinClockEngine

--- BerlinClockTests/MockBerlinClockEngine.swift
+++ BerlinClockTests/MockBerlinClockEngine.swift
@@ -6,39 +6,55 @@ class MockBerlinClockEngine: BerlinClockEngineProtocol {
     func convertDigitalTimeToLamp(time: DigitalTime) -> BerlinClockLamp {
         switch (time.hours, time.minutes, time.seconds) {
         case (8, 23, 2):
-            return BerlinClockLamp(
-                secondsLamp: .red,
-                fiveHoursLamp: [.red, .off, .off, .off],
-                oneHourLamp: [.red, .red, .red, .off],
-                fiveMinutesLamp: [.yellow, .yellow, .red, .yellow, .off, .off, .off, .off, .off, .off, .off],
-                oneMinuteLamp: [.yellow, .yellow, .yellow, .off]
-            )
+            return morningBerlinClockLamp
         case (15, 37, 55):
-            return BerlinClockLamp(
-                secondsLamp: .off,
-                fiveHoursLamp: [.red, .red, .red, .off],
-                oneHourLamp: [.off, .off, .off, .off],
-                fiveMinutesLamp: [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .off, .off, .off, .off],
-                oneMinuteLamp: [.yellow, .yellow, .off, .off]
-            )
+            return afternoonBerlinClockLamp
         case (19, 44, 45):
-            return BerlinClockLamp(
-                secondsLamp: .off,
-                fiveHoursLamp: [.red, .red, .red, .off],
-                oneHourLamp: [.red, .red, .red, .red],
-                fiveMinutesLamp: [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow, .off, .off, .off],
-                oneMinuteLamp: [.yellow, .yellow, .yellow, .yellow]
-            )
+            return eveningBerlinClockLamp
         case (23, 59, 59):
-            return BerlinClockLamp(
-                secondsLamp: .off,
-                fiveHoursLamp: [.red, .red, .red, .red],
-                oneHourLamp: [.red, .red, .red, .off],
-                fiveMinutesLamp: [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow],
-                oneMinuteLamp: [.yellow, .yellow, .yellow, .yellow]
-            )
+            return endOfDayBerlinClockLamp
         default:
             return BerlinClockLamp.empty
         }
+    }
+    
+    private var morningBerlinClockLamp: BerlinClockLamp {
+        BerlinClockLamp(
+            secondsLamp: .red,
+            fiveHoursLamp: [.red, .off, .off, .off],
+            oneHourLamp: [.red, .red, .red, .off],
+            fiveMinutesLamp: [.yellow, .yellow, .red, .yellow, .off, .off, .off, .off, .off, .off, .off],
+            oneMinuteLamp: [.yellow, .yellow, .yellow, .off]
+        )
+    }
+    
+    private var afternoonBerlinClockLamp: BerlinClockLamp {
+        BerlinClockLamp(
+            secondsLamp: .off,
+            fiveHoursLamp: [.red, .red, .red, .off],
+            oneHourLamp: [.off, .off, .off, .off],
+            fiveMinutesLamp: [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .off, .off, .off, .off],
+            oneMinuteLamp: [.yellow, .yellow, .off, .off]
+        )
+    }
+    
+    private var eveningBerlinClockLamp: BerlinClockLamp {
+        BerlinClockLamp(
+            secondsLamp: .off,
+            fiveHoursLamp: [.red, .red, .red, .off],
+            oneHourLamp: [.red, .red, .red, .red],
+            fiveMinutesLamp: [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow, .off, .off, .off],
+            oneMinuteLamp: [.yellow, .yellow, .yellow, .yellow]
+        )
+    }
+    
+    private var endOfDayBerlinClockLamp: BerlinClockLamp {
+        BerlinClockLamp(
+            secondsLamp: .off,
+            fiveHoursLamp: [.red, .red, .red, .red],
+            oneHourLamp: [.red, .red, .red, .off],
+            fiveMinutesLamp: [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow],
+            oneMinuteLamp: [.yellow, .yellow, .yellow, .yellow]
+        )
     }
 }
