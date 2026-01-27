fbd73c30ac4a9fc752cd1c05d21cba0daec9fb4c
2026-DEV2-23 <6wn65x6wns@privaterelay.appleid.com>
2026-01-27 20:57:41 +0530

    refactor code: create a seperate var view for every lamp

--- BerlinClock/View/BerlinClockContentView.swift
+++ BerlinClock/View/BerlinClockContentView.swift
@@ -3,67 +3,83 @@ import SwiftUI
 struct BerlinClockContentView: View {
     var body: some View {
         VStack(spacing: 20) {
-            Circle()
-                .stroke(Color.gray, lineWidth: 5)
-                .background {
-                    Circle()
-                        .fill(.red)
-                }
-                .frame(width: 80)
-            
-            HStack(spacing: 15) {
-                ForEach(0..<4, id:\.self) { index in
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.gray, lineWidth: 5)
-                        .background {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(.red)
-                        }
-                        .frame(height: 100)
-                    
-                }
-            }
-            
-            HStack(spacing: 15) {
-                ForEach(0..<4, id:\.self) { index in
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.gray, lineWidth: 5)
-                        .background(
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(.red)
-                        )
-                        .frame(height: 100)
-                    
-                }
-            }
-            
-            HStack(spacing: 15) {
-                ForEach(0..<11, id:\.self) { index in
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.gray, lineWidth: 5)
-                        .background(
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(.red)
-                        )
-                        .frame(height: 100)
-                    
-                }
-            }
-            
-            HStack(spacing: 15) {
-                ForEach(0..<4, id:\.self) { index in
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.gray, lineWidth: 5)
-                        .background(
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(.red)
-                        )
-                        .frame(height: 100)
-                    
-                }
-            }
+            secondsLampView
+            fiveHoursLamp
+            oneHourLamp
+            fiveMinsLamp
+            oneMinuteLamp
         }
         .padding()
+    }
+    
+    private var secondsLampView: some View {
+        Circle()
+            .stroke(Color.gray, lineWidth: 5)
+            .background {
+                Circle()
+                    .fill(.red)
+            }
+            .frame(width: 80)
+    }
+    
+    private var fiveHoursLamp: some View {
+        HStack(spacing: 15) {
+            ForEach(0..<4, id:\.self) { index in
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 5)
+                    .background {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.red)
+                    }
+                    .frame(height: 100)
+                
+            }
+        }
+    }
+    
+    private var oneHourLamp: some View {
+        HStack(spacing: 15) {
+            ForEach(0..<4, id:\.self) { index in
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 5)
+                    .background(
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.red)
+                    )
+                    .frame(height: 100)
+                
+            }
+        }
+    }
+    
+    private var fiveMinsLamp: some View {
+        HStack(spacing: 15) {
+            ForEach(0..<11, id:\.self) { index in
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 5)
+                    .background(
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.red)
+                    )
+                    .frame(height: 100)
+                
+            }
+        }
+    }
+    
+    private var oneMinuteLamp: some View {
+        HStack(spacing: 15) {
+            ForEach(0..<4, id:\.self) { index in
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 5)
+                    .background(
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.red)
+                    )
+                    .frame(height: 100)
+                
+            }
+        }
     }
 }
 
