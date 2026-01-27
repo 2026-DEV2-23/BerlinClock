f984fc55c350c8513e4046ba85fb19155b4aa41f
2026-DEV2-23 <6wn65x6wns@privaterelay.appleid.com>
2026-01-27 20:28:38 +0530

    add test case handleAfternoonTime, handleEveningTime and handleEndOfDay in time provider

--- BerlinClockTests/BerlinClockTimeProviderTests.swift
+++ BerlinClockTests/BerlinClockTimeProviderTests.swift
@@ -20,6 +20,45 @@ struct BerlinClockTimeProviderTests {
         #expect(original.seconds == seconds)
     }
     
+    @Test("handle morning time")
+    func handleAfternoonTime() throws {
+        let hours: UInt = 15
+        let minutes: UInt = 37
+        let seconds: UInt = 55
+        let mockDate = getMockData(hours: hours, minutes: minutes, seconds: seconds)
+        let original = try timerProvider.getDigitalTime(from: mockDate)
+        
+        #expect(original.hours == hours)
+        #expect(original.minutes == minutes)
+        #expect(original.seconds == seconds)
+    }
+    
+    @Test("handle evening time")
+    func handleEveningTime() throws {
+        let hours: UInt = 19
+        let minutes: UInt = 44
+        let seconds: UInt = 45
+        let mockDate = getMockData(hours: hours, minutes: minutes, seconds: seconds)
+        let original = try timerProvider.getDigitalTime(from: mockDate)
+        
+        #expect(original.hours == hours)
+        #expect(original.minutes == minutes)
+        #expect(original.seconds == seconds)
+    }
+    
+    @Test("handle end of day")
+    func handleEndOfDay() throws {
+        let hours: UInt = 23
+        let minutes: UInt = 59
+        let seconds: UInt = 59
+        let mockDate = getMockData(hours: hours, minutes: minutes, seconds: seconds)
+        let original = try timerProvider.getDigitalTime(from: mockDate)
+        
+        #expect(original.hours == hours)
+        #expect(original.minutes == minutes)
+        #expect(original.seconds == seconds)
+    }
+    
     private func getMockData(hours: UInt, minutes: UInt, seconds: UInt) -> Date {
         var dateComponent = DateComponents()
         dateComponent.hour = Int(hours)
